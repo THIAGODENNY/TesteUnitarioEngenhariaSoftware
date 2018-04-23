39cf5135678c4f04847a3571f598de93add2615f
Thiago Denny <thiagodenny@outlook.com>
2018-04-23 00:53:35 -0300

    Update Banco.java

--- Banco/src/Banco.java
+++ Banco/src/Banco.java
@@ -2,26 +2,20 @@
 public class Banco {
 	
 	public static void main(String[] args){
-		
-		
+				
 		//cenario
 		Cliente vanessa = new Cliente("Vanessa",1000);
 		Cliente thiago = new Cliente("Thiago",2000);
 		
 		//acao
-		vanessa.Transfere(thiago, 100);
-		
+		vanessa.Transfere(thiago, 100);		
 		
 		//validacao
-		
 		double valorThiagoEsperado = 2100;
 		double valorVanessaEsperado = 900;
 		
 		System.out.println(vanessa.getSaldo());
-		System.out.println(thiago.getSaldo());
-		
-		
-		
+		System.out.println(thiago.getSaldo());	
 	}
 
 }
